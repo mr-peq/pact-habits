45205bae72e87eb346f032083b72c04461073804
Joseph Lugand <josephlugand@proton.me>
2023-11-28 19:12:30 +0100

    dashboard3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def dashboard
-    @pacts = Pact.all
+    # All user_pacts for the current user:
+    @user_pacts = current_user.user_pacts
+
+    # And to separate them based on their status:
+    @ongoing_pacts = @user_pacts.where(status: :ongoing).map(&:pact)
+    @achieved_pacts = @user_pacts.where(status: :achieved).map(&:pact)
+    @failed_pacts = @user_pacts.where(status: :failed).map(&:pact)
   end
 
   def account
