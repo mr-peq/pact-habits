c4190dd51c41cfbe8306a9f62fc8b72ab258adb8
Ismael Sentissi <ismael.sentissi@gmail.com>
2023-11-28 10:56:49 +0100

    create and new

--- app/models/pact.rb
+++ app/models/pact.rb
@@ -1,2 +1,17 @@
 class Pact < ApplicationRecord
+  CATEGORIES = ['Run', 'Cycle', 'Walk', 'Hike']
+  WEEKDAYS = {
+    0 => 'Sunday',
+    1 => 'Monday',
+    2 => 'Tuesday',
+    3 => 'Wednesday',
+    4 => 'Thursday',
+    5 => 'Friday',
+    6 => 'Saturday'
+  }
+
+  has_many :user_pacts
+  has_many :users, through: :user_pacts
+
+  validates :category, inclusion: { in: CATEGORIES }
 end
