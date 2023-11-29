8a0c22105a450ff6eab84112168861e299214ca5
Theodore Saulnier <theodore1712@yahoo.fr>
2023-11-29 16:42:19 +0100

    Basic style for challenge cards

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,4 +1,8 @@
 class ChallengesController < ApplicationController
   def index
+    # UserPacts for which the user_id is not current_user_id
+    not_user_pacts = UserPact.where.not(user_id: current_user.id)
+    # + only the ones for which the pact is a challenge
+    @not_user_challenges = not_user_pacts.includes(:pact).where(pacts: { challenge: true }).uniq
   end
 end
