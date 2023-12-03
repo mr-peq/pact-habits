2816a9106fa5b6a44e3c588fc81c590bbfee342c
Theodore Saulnier <theodore1712@yahoo.fr>
2023-12-03 21:59:30 +0100

    xp bars & avatar upgrade functionnal

--- app/controllers/avatar_controller.rb
+++ app/controllers/avatar_controller.rb
@@ -6,6 +6,7 @@ class AvatarController < ApplicationController
     @missing_skills = Skill
       .joins("LEFT JOIN avatar_skills ON skills.id = avatar_skills.skill_id AND avatar_skills.avatar_id = #{@avatar.id}")
       .where("avatar_skills.skill_id IS NULL")
+    @fill_percentage = (@avatar.xp.to_f / @avatar.level.to_next).round(3) * 100
   end
 
   def update
--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -4,4 +4,8 @@ class Avatar < ApplicationRecord
 
   has_many :avatar_skills, dependent: :destroy
   has_many :skills, through: :avatar_skills
+
+  def to_next_level
+    level.to_next - xp
+  end
 end
