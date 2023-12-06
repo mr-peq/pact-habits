a29c2ef7479ad311baf257cd6480d0444eb4f9ea
Theodore Saulnier <theodore1712@yahoo.fr>
2023-12-06 10:04:07 +0100

    Changed migrations

--- db/migrate/20231130161850_create_avatars.rb
+++ db/migrate/20231130161850_create_avatars.rb
@@ -2,15 +2,18 @@ class CreateAvatars < ActiveRecord::Migration[7.1]
   def change
     create_table :avatars do |t|
       t.references :user, null: false, foreign_key: true
-      t.integer :level, default: 1
+      t.references :level, null: false, foreign_key: true
+      t.integer :upgrade_points, default: 4
       t.integer :health, default: 100
       t.integer :attack, default: 10
       t.integer :crit_rate, default: 2
       t.integer :magic_power, default: 10
+      t.integer :magic_defense, default: 10
       t.integer :defense, default: 10
       t.integer :mana, default: 100
       t.integer :speed, default: 10
       t.integer :stamina, default: 10
+      t.integer :xp, default: 0
 
       t.timestamps
     end
--- db/migrate/20231203175422_remove_level_from_avatars.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLevelFromAvatars < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :avatars, :level, :integer
-  end
-end
