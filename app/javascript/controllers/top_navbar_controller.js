58937301ebb978c6b5868d2c8c5dc68e92439a5d
Theodore Saulnier <theodore1712@yahoo.fr>
2023-12-07 16:28:38 +0100

    ...

--- app/javascript/controllers/top_navbar_controller.js
+++ app/javascript/controllers/top_navbar_controller.js
@@ -1,25 +1,25 @@
-// import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus"
 
-// // Connects to data-controller="top-navbar"
-// export default class extends Controller {
-//   static targets = ["navbar"]
+// Connects to data-controller="top-navbar"
+export default class extends Controller {
+  static targets = ["navbar"]
 
-//   connect() {
-//     console.log("top-navbar")
-//     this.lastScrollTop = 0;
-//     window.addEventListener('scroll', this.handleScroll.bind(this));
-//   }
+  connect() {
+    console.log("top-navbar")
+    this.lastScrollTop = 0;
+    window.addEventListener('scroll', this.handleScroll.bind(this));
+  }
 
-//   handleScroll() {
-//     let currentScroll = window.scrollY || document.documentElement.scrollTop;
+  handleScroll() {
+    let currentScroll = window.scrollY || document.documentElement.scrollTop;
 
-//     if (currentScroll > this.lastScrollTop) {
-//       console.log('Scrolling down');
-//       this.navbarTarget.classList.add('navbar-hidden');
-//     } else {
-//       console.log('Scrolling up');
-//       this.navbarTarget.classList.remove('navbar-hidden');
-//     }
-//     this.lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
-//   }
-// }
+    if (currentScroll > this.lastScrollTop) {
+      console.log('Scrolling down');
+      this.navbarTarget.classList.add('navbar-hidden');
+    } else {
+      console.log('Scrolling up');
+      this.navbarTarget.classList.remove('navbar-hidden');
+    }
+    this.lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+  }
+}
