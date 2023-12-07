322e84157a17494cff371f0719f5ba876dea5cf1
Theodore Saulnier <theodore1712@yahoo.fr>
2023-12-07 11:12:00 +0100

    Seeds for demo

--- app/javascript/controllers/circle_timer_controller.js
+++ app/javascript/controllers/circle_timer_controller.js
@@ -15,8 +15,13 @@ export default class extends Controller {
   setTimer() {
     const totalTime = new Date(+this.deadlineTarget.innerText) - new Date(+this.createdAtTarget.innerText);
     const timeLeft = new Date(+this.deadlineTarget.innerText) - Date.now();
-    const fillPercentage = Math.round((timeLeft / totalTime) * 100);
-    this.element.style = `background: linear-gradient(white, white) content-box no-repeat, conic-gradient(#4ECDC4 ${fillPercentage}%, 0, #A6ACBA ) border-box;`;
-    console.log(this.element.style.background);
+    const fillPercentage = (Math.round((timeLeft / totalTime) * 100)) -80;
+    let fillColor;
+    if (fillPercentage <= 20) {
+      fillColor = "rgb(255, 133, 4)";
+    } else {
+      fillColor = "#4ECDC4";
+    }
+    this.element.style = `background: linear-gradient(white, white) content-box no-repeat, conic-gradient(${fillColor} ${fillPercentage}%, 0, #A6ACBA ) border-box;`;
   }
 }
