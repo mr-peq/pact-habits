4b5947ea8fa35e1cbfb5cb81bcd0a05457c510ca
Ismael Sentissi <ismael.sentissi@gmail.com>
2023-12-07 11:00:33 +0100

    pwa and color form

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,6 +75,5 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = false
 
-  config.hosts.clear
   config.hosts << /[a-z0-9\-\.]+\.ngrok-free\.app/
 end
